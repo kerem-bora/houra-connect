631e9a580398d8b7526bf5bec4a0539c7080dbb3
kerem-bora <kerembora@yahoo.com>
2026-02-14 15:23:16 +0300

    auth system with onchainkit 8

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    // Build sırasında TS hatalarını görmezden gelir
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Build sırasında ESLint hatalarını görmezden gelir
     ignoreDuringBuilds: true,
   },
 };
